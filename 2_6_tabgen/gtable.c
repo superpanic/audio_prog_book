4064585b9c9ba2b6e12ce67cf9213b07159c9294
Fredrik Josefsson <fredrik@superpanic.com>
2018-03-05 07:58:02 +0100

    added generic table generation

--- 2_6_tabgen/gtable.c
+++ 2_6_tabgen/gtable.c
@@ -1,7 +1,10 @@
 // gtable.c
 #include <stdlib.h>
+#include <math.h>
 #include "gtable.h"
 #include "../include/common.h"
+
+static void norm_gtable(GTABLE* gtable);
 
 GTABLE *new_sine(uint32_t length) {
 	uint32_t i;
@@ -24,6 +27,79 @@ GTABLE *new_sine(uint32_t length) {
 	return gtable;
 }
 
+GTABLE *new_triangle(uint32_t length, uint32_t nharms) {
+	uint32_t i, j;
+	double step, amp, maxamp;
+	double *table;
+	uint32_t harmonic = 1;
+	GTABLE *gtable = NULL;
+
+	if(length == 0 || nharms == 0 || nharms >= length/2) return NULL;
+
+	gtable = (GTABLE *) malloc(sizeof(GTABLE));
+	if(gtable == NULL) return NULL;
+
+	table = (double *) malloc( (length+1) * sizeof(double) ); // +1 for the guard point
+
+	if(table == NULL) {
+		free(gtable);
+		return NULL;
+	}
+	// reset all to 0.0
+	for(i=0; i<length; i++) table[i] = 0.0;
+	step = TWOPI / length;
+	
+	// generate the triangle wave
+	for(i=0;i<nharms;i++) {
+		amp = 1.0 / (harmonic * harmonic);
+		for(j=0;j<length;j++) table[j] += amp * cos(step * harmonic * j);
+		harmonic += 2;
+	}
+	// normalize table
+	for(i=0; i<length; i++) {
+		amp = fabs(table[i]);
+		if(maxamp < amp) maxamp = amp;
+	}
+	maxamp = 1.0 / maxamp;
+	for(i=0; i<length; i++) table[i] *= maxamp;
+	table[i] = table[0]; // guard point
+	gtable->length = length;
+	gtable->table = table;
+	return gtable;
+
+}
+
+// new generic gtable
+GTABLE *new_gtable(uint32_t length) {
+	uint32_t i;
+	GTABLE* gtable = NULL;
+	if(length == 0) return NULL;
+	gtable = (GTABLE*) malloc(sizeof(GTABLE));
+	if(gtable == NULL) return NULL;
+	gtable->table = (double*) malloc((length + 1 * sizeof(double)));
+	if(gtable->table == NULL) {
+		free(gtable);
+		return NULL;
+	}
+	gtable->length = length;
+	for(i=0;i<=length;i++) gtable->table[i] = 0.0;
+	return gtable;
+}
+
+// normalize the values in a gtable to max 1.0
+static void norm_gtable(GTABLE* gtable) {
+	uint32_t i;
+	double val;
+	double maxamp = 0.0;
+	for(i=0; i<gtable->length; i++) {
+		val = fabs(gtable->table[i]);
+		if(maxamp < val) maxamp = val;
+	}
+	maxamp = 1.0 / maxamp;
+	for(i=0; i<gtable->length; i++) gtable->table[i] *= maxamp;
+	gtable->table[i] = gtable->table[0];
+}
+
 // gtable destruction
 void gtable_free(GTABLE** gtable) {
 	if(gtable && *gtable &&(*gtable)->table) {
@@ -33,7 +109,7 @@ void gtable_free(GTABLE** gtable) {
 	}
 }
 
-OSCIL* new_oscilt(double srate, const GTABLE* gtable, double phase) {
+OSCILT *new_oscilt(double srate, const GTABLE* gtable, double phase) {
 	OSCILT* p_osc;
 	// do we have a good gtable?
 	if(gtable == NULL || gtable->table == NULL || gtable->length == 0) return NULL;
@@ -76,8 +152,7 @@ double tabitick(OSCILT* p_osc, double freq) {
 	double *table = p_osc->gtable->table;
 	if(p_osc->osc.curfreq != freq) {
 		p_osc->osc.curfreq = freq;
-		p_osc->osc.incr = p_osc->sizeovrsr;
-		p_osc->osc.curfreq;
+		p_osc->osc.incr = p_osc->sizeovrsr * p_osc->osc.curfreq;
 	}
 	frac = curphase - base_index;
 	val = table[base_index];
@@ -89,3 +164,4 @@ double tabitick(OSCILT* p_osc, double freq) {
 	p_osc->osc.curphase = curphase;
 	return val;
 }
+
--- 2_6_tabgen/gtable.h
+++ 2_6_tabgen/gtable.h
@@ -19,6 +19,12 @@ typedef struct t_tab_oscil {
 } OSCILT;
 
 GTABLE *new_sine(uint32_t length);
+GTABLE *new_triangle(uint32_t length, uint32_t nharms);
+GTABLE *new_gtable(uint32_t length);
 void gtable_free(GTABLE** gtable);
+
+OSCILT *new_oscilt(double srate, const GTABLE* gtable, double phase);
+double tabtick(OSCILT* p_osc, double freq);
+double tabitick(OSCILT* p_osc, double freq);
 
 #endif
